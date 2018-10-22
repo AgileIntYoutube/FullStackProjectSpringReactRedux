580af1bddd848ae3c1169de86d4ef3364909860f
AgileIntelligence <io.agileintelligence@gmail.com>
2018-10-21 21:24:16 -0400

    initial react project

---
+++ project-board-react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to the project board</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
