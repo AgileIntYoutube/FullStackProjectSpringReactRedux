77317111284aa5a199c3a483585a3137f43c4e93
AgileIntelligence <io.agileintelligence@gmail.com>
2018-10-27 08:08:06 -0400

    depedencies

--- project-board-react/src/App.js
+++ project-board-react/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to the project board</h1>
+        <h1 className="alert alert-warning">Welcome to the project board</h1>
       </div>
     );
   }
