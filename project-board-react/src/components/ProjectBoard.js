fcacf35818d8ad309bdb723cf15050e00c285d5d
AgileIntelligence <io.agileintelligence@gmail.com>
2018-11-03 11:13:50 -0400

    display on board part 1

--- project-board-react/src/components/ProjectBoard.js
+++ project-board-react/src/components/ProjectBoard.js
@@ -10,6 +10,43 @@ class ProjectBoard extends Component {
     this.props.getBacklog();
   }
   render() {
+    const { project_tasks } = this.props.project_tasks;
+
+    let BoardContent;
+    let todoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
+
+    const BoardAlgorithm = project_tasks => {
+      if (project_tasks.length < 1) {
+        return (
+          <div className="alert alert-info text-center" role="alert">
+            No Project Tasks on this board
+          </div>
+        );
+      } else {
+        const tasks = project_tasks.map(project_task => (
+          <ProjectTaskItem key={project_task.id} project_task={project_task} />
+        ));
+
+        for (let i = 0; i < tasks.length; i++) {
+          if (tasks[i].props.project_task.status === "TO_DO") {
+            todoItems.push(tasks[i]);
+          }
+
+          if (tasks[i].props.project_task.status === "IN_PROGRESS") {
+            inProgressItems.push(tasks[i]);
+          }
+
+          if (tasks[i].props.project_task.status === "DONE") {
+            doneItems.push(tasks[i]);
+          }
+        }
+      }
+    };
+
+    BoardAlgorithm(project_tasks);
+
     return (
       <div className="container">
         <Link to="/addProjectTask" className="btn btn-primary mb-3">
@@ -28,10 +65,10 @@ class ProjectBoard extends Component {
               {
                 // <!-- SAMPLE PROJECT TASK STARTS HERE -->
               }{" "}
-              <ProjectTaskItem />
               {
                 // <!-- SAMPLE PROJECT TASK ENDS HERE -->
               }{" "}
+              {todoItems}
             </div>
             <div className="col-md-4">
               <div className="card text-center mb-2">
@@ -43,8 +80,7 @@ class ProjectBoard extends Component {
                 //     <!-- SAMPLE PROJECT TASK STARTS HERE -->
                 // <!-- SAMPLE PROJECT TASK ENDS HERE -->
               }
-
-              <ProjectTaskItem />
+              {inProgressItems}
             </div>
             <div className="col-md-4">
               <div className="card text-center mb-2">
@@ -56,6 +92,7 @@ class ProjectBoard extends Component {
                 //     <!-- SAMPLE PROJECT TASK STARTS HERE -->
                 // <!-- SAMPLE PROJECT TASK ENDS HERE -->
               }
+              {doneItems}
             </div>
           </div>
         </div>
