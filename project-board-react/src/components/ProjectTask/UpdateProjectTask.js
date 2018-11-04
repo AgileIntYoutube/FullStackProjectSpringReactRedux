5001846ef10540f58d4a9edb8b392d76cc2267db
AgileIntelligence <io.agileintelligence@gmail.com>
2018-11-04 07:13:27 -0500

    final video

--- project-board-react/src/components/ProjectTask/UpdateProjectTask.js
+++ project-board-react/src/components/ProjectTask/UpdateProjectTask.js
@@ -1,16 +1,63 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import classnames from "classnames";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import { getProjectTask } from "../../actions/projectTaskActions";
+import {
+  getProjectTask,
+  addProjectTask
+} from "../../actions/projectTaskActions";
 
 class UpdateProjectTask extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      summary: "",
+      acceptanceCriteria: "",
+      status: "",
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+
+    const { id, summary, acceptanceCriteria, status } = nextProps.project_task;
+
+    this.setState({
+      id,
+      summary,
+      acceptanceCriteria,
+      status
+    });
+  }
+
   componentDidMount() {
     const { pt_id } = this.props.match.params;
     this.props.getProjectTask(pt_id);
   }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const updatedTask = {
+      id: this.state.id,
+      summary: this.state.summary,
+      acceptanceCriteria: this.state.acceptanceCriteria,
+      status: this.state.status
+    };
+
+    this.props.addProjectTask(updatedTask, this.props.history);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
   render() {
+    const { errors } = this.state;
     return (
       <div className="addProjectTask">
         <div className="container">
@@ -22,26 +69,37 @@ class UpdateProjectTask extends Component {
               <h4 className="display-4 text-center">
                 Add /Update Project Task
               </h4>
-              <form>
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.summary
+                    })}
                     name="summary"
                     placeholder="Project Task summary"
+                    value={this.state.summary}
+                    onChange={this.onChange}
                   />
+                  {errors.summary && (
+                    <div className="invalid-feedback">{errors.summary}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <textarea
                     className="form-control form-control-lg"
                     placeholder="Acceptance Criteria"
                     name="acceptanceCriteria"
+                    value={this.state.acceptanceCriteria}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
                   <select
                     className="form-control form-control-lg"
                     name="status"
+                    value={this.state.status}
+                    onChange={this.onChange}
                   >
                     <option value="">Select Status</option>
                     <option value="TO_DO">TO DO</option>
@@ -65,15 +123,16 @@ class UpdateProjectTask extends Component {
 UpdateProjectTask.propTypes = {
   project_task: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired,
-  getProjectTask: PropTypes.func.isRequired
+  getProjectTask: PropTypes.func.isRequired,
+  addProjectTask: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
-  project_task: state.project_task,
+  project_task: state.project_task.project_task,
   errors: state.errors
 });
 
 export default connect(
   mapStateToProps,
-  { getProjectTask }
+  { getProjectTask, addProjectTask }
 )(UpdateProjectTask);
